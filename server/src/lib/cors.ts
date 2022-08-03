e131c8d8cc3e813c2e39f41651af7edcbe980bbe
soamati <matiassdp96@gmail.com>
2022-08-03 03:17:08 -0300

    some cors issues :bug: & UserInput validation

--- server/src/lib/cors.ts
+++ server/src/lib/cors.ts
@@ -2,7 +2,7 @@ import corsMiddleware from "cors";
 
 export function cors() {
   return corsMiddleware({
-    origin: ["https://studio.apollographql.com"],
+    origin: ["https://studio.apollographql.com", "http://localhost:3000"],
     credentials: true,
   });
 }
--- server/src/modules/user/types.ts
+++ server/src/modules/user/types.ts
@@ -1,15 +1,17 @@
 import { User } from "@prisma/client";
-import { Matches, MinLength } from "class-validator";
+import { Matches, MinLength, NotContains } from "class-validator";
 import { Field, InputType } from "type-graphql";
 
 @InputType()
 export class UserInput implements Partial<User> {
   @Field()
-  @MinLength(3, { message: "Al menos 3 caracteres" })
   @Matches(/^[A-Za-z0-9]+$/, { message: "Solo letras, números y _" })
+  @NotContains(" ", { message: "Sin espacios" })
+  @MinLength(3, { message: "Al menos 3 caracteres" })
   username: string;
 
   @Field()
+  @NotContains(" ", { message: "Sin espacios" })
   @MinLength(3, { message: "Al menos 3 caracteres" })
   password: string;
 }
