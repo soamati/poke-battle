8398c9723a135fabf9fb96a76bac551f0f0ec90a
Matias Ruiz <matiassdp96@gmail.com>
2022-10-12 01:17:03 -0300

    add data sources on about page :tada:

--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -1,37 +1,57 @@
 import React from "react";
 import Page from "@/layout/Page";
-import { Center, HStack, Icon, Stack, Text, Button } from "@chakra-ui/react";
+import {
+  Center,
+  HStack,
+  Icon,
+  Stack,
+  Text,
+  Button,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
 import { FiGithub, FiLinkedin } from "react-icons/fi";
 import ExternalLink from "@/components/ExternalLink";
 import Link from "next/link";
-import { ArrowBackIcon } from "@chakra-ui/icons";
 
 const AboutPage = () => {
   return (
     <Page full showNav={false} showHeader={false}>
       <Center flex={1}>
-        <Stack spacing={4} align="center">
-          <Text textAlign="center">
-            Aplicación creada con fines de aprendizaje
-          </Text>
+        <Stack spacing={6} align="center">
+          <Stack>
+            <Text textAlign="center" color="gray.400">
+              Aplicación creada con fines de aprendizaje
+            </Text>
+            <Text textAlign="center">Matias Ruiz</Text>
+            <HStack justify="center" spacing={4}>
+              <ExternalLink href="https://github.com/soamati">
+                <Icon as={FiGithub} cursor="pointer" />
+              </ExternalLink>
+              <ExternalLink href="https://www.linkedin.com/in/matiruizsh/">
+                <Icon as={FiLinkedin} cursor="pointer" />
+              </ExternalLink>
+            </HStack>
+          </Stack>
 
-          <Text textAlign="center">Matias Ruiz</Text>
-
-          <HStack justify="center" spacing={4}>
-            <ExternalLink href="https://github.com/soamati">
-              <Icon as={FiGithub} cursor="pointer" />
-            </ExternalLink>
-            <ExternalLink href="https://www.linkedin.com/in/matiruizsh/">
-              <Icon as={FiLinkedin} cursor="pointer" />
-            </ExternalLink>
-          </HStack>
+          <Stack>
+            <Text textAlign="center" color="gray.400">
+              Fuentes de información:
+            </Text>
+            <HStack justify="center" spacing={4}>
+              <ExternalLink href="https://pokeapi.co/">
+                <ChakraLink>PokéAPI</ChakraLink>
+              </ExternalLink>
+              <ExternalLink href="https://pokemon.fandom.com/es/wiki/Primera_generación">
+                <ChakraLink>WikiDex</ChakraLink>
+              </ExternalLink>
+              <ExternalLink href="https://github.com/soamati/poke-parser">
+                <ChakraLink>WebScraper</ChakraLink>
+              </ExternalLink>
+            </HStack>
+          </Stack>
 
           <Link href="/" passHref>
-            <Button
-              leftIcon={<ArrowBackIcon />}
-              colorScheme="yellow"
-              variant="ghost"
-            >
+            <Button colorScheme="yellow" variant="ghost" size="sm">
               Volver al inicio
             </Button>
           </Link>
